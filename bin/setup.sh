a9e60fb52366a710306307eb8b9ed733d7196346
kunado <katsusand1@gmail.com>
2018-11-10 07:32:52 +0900

    pull from develop branches while setting up

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+# register submodules
 git submodule init
 git submodule update
+
+# pull from develop branches
+REPOSITORIES=(backend/rails frontend/nuxt)
+for repo in ${REPOSITORIES[@]}; do
+	cd $repo
+	git checkout develop
+	git pull
+	cd ../../
+done
+
+# build
 docker-compose build
 docker-compose run backend bundle
 docker-compose run frontend yarn
