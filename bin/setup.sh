3a78bb1ff47cbcdc27be46e2b134e77f3e0df789
kunado <katsusand1@gmail.com>
2018-11-10 07:10:11 +0900

    build and migrate when executing setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git submodule init
 git submodule update
+docker-compose build
+docker-compose run backend bundle
+docker-compose run frontend yarn
+docker-compose run backend rails db:migrate
